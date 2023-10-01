b7017ad2588366c2d95b781a081913a78275bea8
Divine Edwin <edwindivine5@gmail.com>
2023-10-01 16:08:27 +0100

    update: implement lazy loading and suspense

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,17 +1,20 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Homepage from '../pages/Homepage'
-import MovieDetails from '../pages/MovieDetails'
-import Errorpage from '../pages/Errorpage'
+const Homepage = lazy(() => import('../pages/Homepage'))
+const MovieDetails = lazy(() => import('../pages/MovieDetails'))
+const Errorpage = lazy(() => import('../pages/Errorpage'))
 
 const RouterLink = () => {
     return (
         <>
             <Router>
-                <Routes>
-                    <Route path='/' element={<Homepage />} />
-                    <Route path='/movies/:id' element={<MovieDetails />} />
-                    <Route path='*' element={<Errorpage />} />
-                </Routes>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path='/' element={<Homepage />} />
+                        <Route path='/movies/:id' element={<MovieDetails />} />
+                        <Route path='*' element={<Errorpage />} />
+                    </Routes>
+                </Suspense>
             </Router>
         </>
     )
