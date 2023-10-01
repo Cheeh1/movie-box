7d94242b97c4df8199084d19c5e7e8709a5a8ba3
Divine Edwin <edwindivine5@gmail.com>
2023-10-01 13:59:41 +0100

    fix: deleted popular movies page

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -28,7 +28,7 @@ const MovieList = () => {
                             <div className="flex justify-between">
                                 <p className="text-gray-900 font-bold text-xl lg:text-2xl">Popular Movie</p>
                                 <div className="flex gap-1 items-center">
-                                    <Link to="/popular" className="text-rose-700 text-sm">See More</Link>
+                                    <p className="text-rose-700 text-sm cursor-pointer">See More</p>
                                     <img className="w-[15px] mt-[5px]" src={right} alt="right-logo" />
                                 </div>
                             </div>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from '../pages/Homepage'
 import MovieDetails from '../pages/MovieDetails'
-import PopularMovie from '../pages/PopularMovie'
 import Errorpage from '../pages/Errorpage'
 
 const RouterLink = () => {
@@ -11,7 +10,6 @@ const RouterLink = () => {
                 <Routes>
                     <Route path='/' element={<Homepage />} />
                     <Route path='/movies/:id' element={<MovieDetails />} />
-                    <Route path='/popular' element={<PopularMovie />} />
                     <Route path='*' element={<Errorpage />} />
                 </Routes>
             </Router>
